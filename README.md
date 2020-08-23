16d003bb07051e19d853eb31a8e08243ebd88ec1
date_cocoa <co.yo.fo@gmail.com>
2020-08-23 20:14:50 +0900

    add : infected

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # corona
-コロナウイウルスに関するスクリプト。./src/配下で実行すること。
+コロナウイウルスに関するスクリプト。./src/配下で実行すること。\
+
+## 実行順
+### 発熱等相談件数の場合
+```zsh
+python get_data_soudan.py
+Rscript plot_soudan.R
+open ../img/soudan.png
+```
+
+### 報告感染者数の場合
+```zsh
+Rscript plot_infected.R
+open ../img/infected.png
+```
---
+++ src/plot_infected.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+
+url <- 'https://stopcovid19.metro.tokyo.lg.jp/data/130001_tokyo_covid19_patients.csv'
+data <- read_csv(url)
+
+my_theme <-
+  theme_bw(base_family = "HiraKakuProN-W3") + 
+  theme(axis.text = element_text(size = 10),
+        axis.title = element_text(size = 10), axis.ticks.length=unit(0.5, "cm"))
+theme_set(my_theme) # set theme
+
+title <- paste0('報告感染者数_', Sys.Date())
+
+g <- 
+  data %>% 
+  select(公表_年月日, 曜日) %>% 
+  group_by(公表_年月日, 曜日) %>% 
+  count() %>% 
+  ggplot() + 
+  geom_line(aes(x = 公表_年月日,  y = n, color = 曜日)) + 
+  scale_color_discrete(breaks=c('月', '火', '水', '木', '金', '土', '日')) +
+  ggtitle(title)
+
+ggsave('../img/infected.png', width = 9, height = 5)
